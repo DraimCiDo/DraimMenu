71a1b6b952d3663cf81ba50a42c15a3c96da5ce9
DanichMan <danilaorlov4@gmail.com>
2021-07-30 23:59:48 +0300

    Версия 1.5 (Обновление /draimmenu version)

--- src/main/java/me/draimgoose/draimmenu/commands/CommandGUIVersion.java
+++ src/main/java/me/draimgoose/draimmenu/commands/CommandGUIVersion.java
@@ -17,15 +17,32 @@ public class CommandGUIVersion implements CommandExecutor {
         if (label.equalsIgnoreCase("dmv") || label.equalsIgnoreCase("draimmenuversion") || label.equalsIgnoreCase("dmenuv")) {
             if(args.length == 0) {
                 if (sender.hasPermission("draimmenu.version")) {
+                    String latestVersion = plugin.updater.getLatestVersion(false);
                     sender.sendMessage(plugin.tex.colour(plugin.tag));
                     sender.sendMessage(ChatColor.GREEN + "Текущая версия " + ChatColor.GRAY + plugin.getDescription().getVersion());
+                    sender.sendMessage(ChatColor.GREEN + "Последняя версия " + ChatColor.GRAY + latestVersion);
                     sender.sendMessage(ChatColor.GRAY + "-------------------");
                     sender.sendMessage(ChatColor.GREEN + "Разработчик " + ChatColor.GRAY + "DraimGooSe");
                     sender.sendMessage(ChatColor.GREEN + "Команда " + ChatColor.GRAY + "/dm");
                 } else {
                     sender.sendMessage(plugin.tex.colour(plugin.tag + plugin.config.getString("config.format.perms")));
                 }
+            }else if(args.length == 1){
+                if (sender.hasPermission("draimmenu.update")) {
+                    if (args[0].equals("отмена")) {
+                        plugin.updater.downloadVersionManually = null;
+                        sender.sendMessage(plugin.tex.colour(plugin.tag + ChatColor.GREEN + "Новая версия не будет загружаться при перезапуске."));
+                    } else {
+                        plugin.updater.downloadVersionManually = args[0];
+                        sender.sendMessage(plugin.tex.colour(plugin.tag + ChatColor.GREEN + "Скачивание версии " + ChatColor.GRAY + args[0] + ChatColor.GREEN + " при перезагрузке сервера."));
+                    }
+                }else{
+                    sender.sendMessage(plugin.tex.colour(plugin.tag + plugin.config.getString("config.format.perms")));
+                }
+            }else{
+                sender.sendMessage(plugin.tex.colour(plugin.tag + ChatColor.RED + "Используйте: /dmv [update:latest:отмена]"));
             }
+            return true;
         }
         return true;
     }
