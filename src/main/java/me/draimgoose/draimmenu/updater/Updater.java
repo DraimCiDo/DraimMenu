31c4508956fde2c306f9206ac5ab9dfe03bfde27
DanichMan <danilaorlov4@gmail.com>
2021-07-31 20:45:30 +0300

    Версия 1.7.8 (Фикс Updater.java)

--- src/main/java/me/draimgoose/draimmenu/updater/Updater.java
+++ src/main/java/me/draimgoose/draimmenu/updater/Updater.java
@@ -20,12 +20,9 @@ public class Updater implements Listener {
         this.plugin = pl;
     }
 
-    //если установлено на что-то, плуг загрузит эту версию при перезапуске.
-    //Это может быть номер версии, "latest" или "cancel".
     public String downloadVersionManually = null;
     public String catchedLatestVersion = "null";
 
-    //отправляет сообщение об обновлении, когда игрок присоединится к игре с пермишкой
     @EventHandler
     public void joinGame(PlayerJoinEvent e){
         if(e.getPlayer().hasPermission("draimmenu.update") && plugin.config.getBoolean("updater.update-checks")){
@@ -33,38 +30,36 @@ public class Updater implements Listener {
                 new BukkitRunnable() {
                     @Override
                     public void run() {
-                        plugin.tex.sendMessage(e.getPlayer(),ChatColor.YELLOW + "Новое обновление доступно для скачивания!");
+                        plugin.tex.sendMessage(e.getPlayer(),ChatColor.YELLOW + "Доступно новое обновление!");
                         plugin.tex.sendString(e.getPlayer(),ChatColor.YELLOW
-                                + "Текущая версия "
+                                + "Текущая версия - "
                                 + ChatColor.RED + plugin.getDescription().getVersion() + ChatColor.YELLOW
-                                + " Последняя версия " + ChatColor.GREEN + catchedLatestVersion);
+                                + " Последняя версия - " + ChatColor.GREEN + catchedLatestVersion);
                         this.cancel();
                     }
-                }.runTaskTimer(plugin, 30, 1); //20 тиков == 1 сек
+                }.runTaskTimer(plugin, 30, 1);
             }
         }
     }
 
     public boolean githubNewUpdate(boolean sendMessages){
-        //обноление до последней версии
         getLatestVersion(sendMessages);
 
         if(plugin.getDescription().getVersion().contains("-")){
             if(sendMessages) {
-                Bukkit.getConsoleSender().sendMessage("DraimMenu" + ChatColor.GREEN + " Запущен на модифированной версии.");
+                Bukkit.getConsoleSender().sendMessage("(DraimMenu)" + ChatColor.GREEN + " Вы используете кастомную версию.");
             }
             return false;
         }
 
-        //если обновление стоит на true, будет показ о новом обновление
         boolean update = !catchedLatestVersion.equals(plugin.getDescription().getVersion());
 
         if(update){
             if(sendMessages) {
                 Bukkit.getConsoleSender().sendMessage("(DraimMenu)" + ChatColor.GOLD + " ================================================");
-                Bukkit.getConsoleSender().sendMessage("(DraimMenu)" + ChatColor.AQUA + " Доступно обновление для DraimMenu.");
-                Bukkit.getConsoleSender().sendMessage("(DraimMenu)" + " Для скачивания DraimMenu " + ChatColor.GOLD + catchedLatestVersion + ChatColor.WHITE + " используй");
-                Bukkit.getConsoleSender().sendMessage("(DraimMenu)" + ChatColor.WHITE + " команду:" + ChatColor.AQUA + " /dmv latest" + ChatColor.WHITE + " и презапусти сервер");
+                Bukkit.getConsoleSender().sendMessage("(DraimMenu)" + ChatColor.AQUA + " Доступно новое обновление.");
+                Bukkit.getConsoleSender().sendMessage("(DraimMenu)" + " Скачайте " + ChatColor.GOLD + catchedLatestVersion + ChatColor.WHITE + " используя");
+                Bukkit.getConsoleSender().sendMessage("(DraimMenu)" + ChatColor.WHITE + " команду:" + ChatColor.AQUA + " /dmv latest" + ChatColor.WHITE + " и перезапустите сервер");
                 Bukkit.getConsoleSender().sendMessage("(DraimMenu)" + ChatColor.GOLD + " ================================================");
             }
             return true;
@@ -73,35 +68,32 @@ public class Updater implements Listener {
     }
 
     public String getLatestVersion(boolean sendMessages){
-        //проверка при null
         if(catchedLatestVersion.equals("null")){
             catchedLatestVersion = plugin.getDescription().getVersion();
         }
 
-        //использование массива позволяет редактировать его в процессе доработки
         new BukkitRunnable(){
             public void run(){
                 HttpURLConnection connection;
                 try {
-                    connection = (HttpURLConnection) new URL("https://raw.githubusercontent.com/DraimCiDo/DraimMenu/master/src/main/resources/plugin.yml").openConnection();
+                    connection = (HttpURLConnection) new URL("https://raw.githubusercontent.com/DraimCiDo/DraimMenu/master/src/main/resources/plugin.ymll").openConnection();
                     connection.connect();
                     catchedLatestVersion = new BufferedReader(new InputStreamReader(connection.getInputStream())).readLine().split("\\s")[1];
                     connection.disconnect();
                 } catch (IOException ignore) {
-                    Bukkit.getConsoleSender().sendMessage("(DraimMenu)" + ChatColor.RED + " Не удалось получить доступ к github.");
+                    Bukkit.getConsoleSender().sendMessage("(DraimMenu)" + ChatColor.RED + " Не удалось получить доступ к GitHub.");
                 }
             }
         }.runTask(plugin);
 
         if(catchedLatestVersion.contains("-")){
             if(sendMessages) {
-                Bukkit.getConsoleSender().sendMessage("(DraimMenu)" + ChatColor.RED + " Не удается проверить наличие обновлений.");
+                Bukkit.getConsoleSender().sendMessage("(DraimMenu)" + ChatColor.RED + " Не удалось проверить версию.");
             }
         }
         return catchedLatestVersion;
     }
 
-    //имя плагина может быть получено только из основного класса
     public void autoUpdatePlugin(String pluginFileName){
         String latestVersion = catchedLatestVersion;
         String thisVersion = plugin.getDescription().getVersion();
@@ -116,17 +108,13 @@ public class Updater implements Listener {
         }
 
         if(latestVersion.equals(thisVersion) || thisVersion.contains("-")){
-            //не нужно обновлять или запускать пользовательскую версию
             return;
         }
         if (Objects.requireNonNull(plugin.config.getString("updater.auto-update")).equalsIgnoreCase("true")) {
-            //не обновляет плагин автоматически, если он отключен
             return;
         }
         if(Objects.equals(plugin.config.getString("updater.minor-updates-only"), "true")){
-            //обновляет онли те версии, которые не будут ломаться
             if(thisVersion.split("\\.")[1].equals(latestVersion.split("\\.")[1]) && thisVersion.split("\\.")[0].equals(latestVersion.split("\\.")[0])){
-                //первый и второй номера версии одинаковы, обновления: [major.major.minor.minor]
                 downloadFile(latestVersion,pluginFileName);
             }
         }else{
@@ -134,13 +122,12 @@ public class Updater implements Listener {
         }
     }
 
-    // Процесс скачки с гита
     private void downloadFile(String latestVersion, String pluginFileName) {
         BufferedInputStream in = null;
         FileOutputStream fout = null;
 
         try {
-            this.plugin.getLogger().info("Установка нового DraimMenu " + "v" + latestVersion);
+            this.plugin.getLogger().info("Загрузка " + "v" + latestVersion);
             URL fileUrl = new URL("https://github.com/DraimCiDo/DraimMenu/releases/download/" + latestVersion + "/DraimMenu.jar");
             int fileLength = fileUrl.openConnection().getContentLength();
             in = new BufferedInputStream(fileUrl.openStream());
@@ -155,12 +142,12 @@ public class Updater implements Listener {
                 fout.write(data, 0, count);
                 int percent = (int)(downloaded * 100L / (long)fileLength);
                 if (percent % 10 == 0) {
-                    this.plugin.getLogger().info("Загрузка обновления: " + percent + "% из " + fileLength + " байтов.");
+                    this.plugin.getLogger().info("Загрузка: " + percent + "% из " + fileLength + " байтов.");
                 }
             }
-            this.plugin.getLogger().info("Обновление завершено");
+            this.plugin.getLogger().info("Загрузка завершена.");
         } catch (Exception var22) {
-            this.plugin.getLogger().log(Level.WARNING, "Не удалось загрузить обновление.", var22);
+            this.plugin.getLogger().log(Level.WARNING, "Не удалось загрузить последнию версию.", var22);
         } finally {
             try {
                 if (in != null) {
